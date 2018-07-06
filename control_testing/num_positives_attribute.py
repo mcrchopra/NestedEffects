650679193976b7a35a930c60f55625063bf09519
Manu Chopra <mcrchopra@gmail.com>
2018-07-06 08:13:05 -0700

    Deleting Control Testing Folder

--- control_testing/num_positives_attribute.py
+++
@@ -1,6 +0,0 @@
-import pandas
-import csv
-
-binary_matrix = pandas.read_csv("full_event_dataframe_nonans.tab", index_col = 0, sep = "\t")
-num_positives = binary_matrix.sum()
-num_positives.to_csv("num_positives_attribute_without_nans.tab", sep = "\t")
